02de2a0a8a2d787c47b23c98bd6a5d82939be422
vignezz007 <69901350+vignezz007@users.noreply.github.com>
2023-10-19 12:57:21 +0530

    Update add.py

--- add.py
+++ add.py
@@ -5,6 +5,8 @@ num2 = 6.3
 
 # Add two numbers
 sum = num1 + num2
+sub = num1 - num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sub of {0} and {1} is {2}'.format(num1, num2, sum))
